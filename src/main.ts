cf8e98c2ee2a0f5602e91b37aa7834628d265541
Софья <sof@MacBook-Air-Sofa.local>
2024-03-24 12:55:55 +0200

    Add register form

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {bootstrapApplication} from '@angular/platform-browser'
+import {provideRouter} from '@angular/router'
+import {AppComponent} from './app/app.component'
+import {appRoutes} from './app/app.routes'
+import {provideState, provideStore} from '@ngrx/store'
+import {provideStoreDevtools} from '@ngrx/store-devtools'
+import {isDevMode} from '@angular/core'
+import { authFeatureKey, authReducer } from './app/auth/store/reducers'
+import { provideHttpClient } from '@angular/common/http'
+import { provideEffects } from '@ngrx/effects'
+import * as authEffects from './app/auth/store/effects'
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(appRoutes),
+    provideStore(),
+    provideState(authFeatureKey, authReducer),
+    provideEffects(authEffects),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
+  ],
+})
